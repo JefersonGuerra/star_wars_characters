2bd104e07354033d74f06ce4eb959815ca9418f9
JefersonGuerra <jeferson.ctbbr@gmail.com>
2024-07-07 15:43:23 -0300

    add environment variables cypress

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,6 +1,6 @@
 describe('Navigation', () => {
   it('should navigate to the favorites page', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit(Cypress.env('host'))
     cy.get('a[href*="favorites"]').click()
     cy.url().should('include', '/favorites')
   })
@@ -8,7 +8,7 @@ describe('Navigation', () => {
 
 describe('API', () => {
   it('should find name characters in the list', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit(Cypress.env('host'))
     cy.request('https://swapi.dev/api/people').then((response) => {
       const data = response.body.results;
       cy.get('.list-characters').each(($el, index) => {
@@ -20,15 +20,15 @@ describe('API', () => {
 
 describe('Favorite', () => {
   it('should click button favorite and verify page', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit(Cypress.env('host'))
     cy.get('.list-characters').each(($el, index) => {
       cy.wrap($el).find('svg').click()
     })
 
-    cy.visit('http://localhost:3000/favorites')
+    cy.visit(`${Cypress.env('host')}/favorites`)
 
     cy.getAllLocalStorage().then((result) => {
-      const local = result['http://localhost:3000']['favoriteCharacters']
+      const local = result[Cypress.env('host')]['favoriteCharacters']
       const data = JSON.parse(local).map(item => item.name)
       cy.get('.list-characters').each(($el, index) => {
         cy.wrap($el).find('h2').contains(data[index])
